bdc907fffbf224b099cbe6bd2138e65eac75ea2a
Pandurang Kamath <pandu1990@gmail.com>
2020-07-11 03:23:49 -0700

    Use the class properties babel plugin to make use of the class properties syntax. We can then declare the state directly instead of doing it in the constructor. Also converting other functions to arrow syntax. In this case the this automatically binds to the instance of the class. So we can remove the manual bind from the constructor.

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -5,16 +5,9 @@ import Options from './Options';
 import AddOption from './AddOption';
 
 class IndecisionApp extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.state = {
-      options: props.options
-    };
-  }
+  state = {
+    options: []
+  };
   componentDidMount() {
     try {
       const json = localStorage.getItem('options');
@@ -32,20 +25,20 @@ class IndecisionApp extends React.Component {
       localStorage.setItem('options', json);
     }
   }
-  handleDeleteOptions() {
+  handleDeleteOptions = () => {
     this.setState(() => ({ options: [] }));
-  }
-  handleDeleteOption(optionToRemove) {
+  };
+  handleDeleteOption = (optionToRemove) => {
     this.setState((prevState) => ({
       options: prevState.options.filter((option) => option !== optionToRemove)
     }));
-  }
-  handlePick() {
+  };
+  handlePick = () => {
     const randomNum = Math.floor(Math.random() * this.state.options.length);
     const option = this.state.options[randomNum];
     alert(option);
-  }
-  handleAddOption(option) {
+  };
+  handleAddOption = (option) => {
     if (!option) {
       return 'Enter valid value to add item';
     } else if (this.state.options.indexOf(option) > -1) {
@@ -54,7 +47,7 @@ class IndecisionApp extends React.Component {
     this.setState((prevState) => ({
       options: prevState.options.concat(option)
     }));
-  }
+  };
   render() {
     const subtitle = 'Put your life in the hands of a computer';
 
@@ -77,9 +70,5 @@ class IndecisionApp extends React.Component {
     );
   }
 }
-
-IndecisionApp.defaultProps = {
-  options: []
-};
 
 export default IndecisionApp;
