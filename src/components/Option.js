bdeb2d9b5b379f8b3592ef4af1522a61cf005e47
Pandurang Kamath <pandu1990@gmail.com>
2020-07-12 16:22:36 -0700

    Styling the buttons.

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,10 +3,11 @@ import React from 'react';
 const Option = (props) => (
   <div>
     {props.optionText}
-    <button onClick={
-      () => props.handleDeleteOption(props.optionText)
-    }>
-      remove
+    <button
+      className="button button--link"
+      onClick={() => props.handleDeleteOption(props.optionText)}
+    >
+      Remove
     </button>
   </div>
 );
