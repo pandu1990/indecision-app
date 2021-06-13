8dff91104b6600802e5aea336024c648a0c64aa0
Pandurang Kamath <pandu1990@gmail.com>
2021-06-13 15:20:13 -0700

    clean start

--- src/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-const rootAppElement = document.getElementById('app');
-
-ReactDOM.render(<IndecisionApp appElement={rootAppElement} />, rootAppElement);
