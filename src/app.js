7712862ad47f07cf79a54dc8f91e211a4fa36094
Pandurang Kamath <pandu1990@gmail.com>
2021-06-13 22:32:22 -0700

    Forms and inputs

--- src/app.js
+++ src/app.js
@@ -3,42 +3,42 @@ console.log('app.js in running');
 const app = {
   title: 'Indecision App',
   subtitle: 'Put your life in the hands of a computer',
-  options: ['One', 'Two']
-}
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {app.subtitle && <p>{app.subtitle}</p>}
-    <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
-  </div>
-);
+  options: []
+};
 
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
+const onFormSubmit = (e) => {
+  e.preventDefault();
+
+  const option = e.target.elements.option.value;
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = '';
+    renderApp();
+  }
 };
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  count = 0;
-  renderCounterApp();
+
+const onRemoveAll = () => {
+  app.options = [];
+  renderApp();
 };
 
 const appRoot = document.getElementById('app');
 
-const renderCounterApp = () => {
-  const templateTwo = (
+const renderApp = () => {
+  const template = (
     <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{(app.options && app.options.length > 0) ? 'Here are your options' : 'No options'}</p>
+      <p>{app.options.length}</p>
+      <button onClick={onRemoveAll}>Remove All</button>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option"/>
+        <button>Add Option</button>
+      </form>
     </div>
   );
-  ReactDOM.render(templateTwo, appRoot);
-};
 
-renderCounterApp();
+  ReactDOM.render(template, appRoot);
+};
+renderApp();
